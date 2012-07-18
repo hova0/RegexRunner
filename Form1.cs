62cb95b867901b0d9ac0a2cb097378cd5277cb46
Jared McManus <hova__0@hotmail.com>
2012-07-18 16:32:48 -0500

    Runs on .net 4, added Sort and Uniq buttons
    Naive Sorting and Uniqueify buttons.  Not recommended for large amounts
    of text.

--- Form1.cs
+++ Form1.cs
@@ -64,6 +64,8 @@ namespace RegExHelper
         private CheckBox IgnoreDups;
         private ToolTip toolTip1;
         private Button button6;
+        private Button button8;
+        private Button button7;
         private IContainer components;
 		
 		public Form1()
@@ -198,6 +200,8 @@ namespace RegExHelper
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.panel3.SuspendLayout();
             this.pane2.SuspendLayout();
             this.panel5.SuspendLayout();
@@ -322,9 +326,9 @@ namespace RegExHelper
             // 
             this.panel7.Controls.Add(this.label_message);
             this.panel7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel7.Location = new System.Drawing.Point(372, 0);
+            this.panel7.Location = new System.Drawing.Point(463, 0);
             this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(164, 24);
+            this.panel7.Size = new System.Drawing.Size(73, 24);
             this.panel7.TabIndex = 99;
             // 
             // label_message
@@ -334,13 +338,15 @@ namespace RegExHelper
             this.label_message.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label_message.Location = new System.Drawing.Point(0, 0);
             this.label_message.Name = "label_message";
-            this.label_message.Size = new System.Drawing.Size(164, 24);
+            this.label_message.Size = new System.Drawing.Size(73, 24);
             this.label_message.TabIndex = 99;
             this.toolTip1.SetToolTip(this.label_message, "Status Message - Will print \"No occurances\" when the \r\nexpression failed to match" +
         ".");
             // 
             // panel8
             // 
+            this.panel8.Controls.Add(this.button8);
+            this.panel8.Controls.Add(this.button7);
             this.panel8.Controls.Add(this.button6);
             this.panel8.Controls.Add(this.button5);
             this.panel8.Controls.Add(this.button4);
@@ -350,7 +356,7 @@ namespace RegExHelper
             this.panel8.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel8.Location = new System.Drawing.Point(0, 0);
             this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(372, 24);
+            this.panel8.Size = new System.Drawing.Size(463, 24);
             this.panel8.TabIndex = 99;
             // 
             // button6
@@ -568,6 +574,28 @@ namespace RegExHelper
             this.toolTip1.InitialDelay = 500;
             this.toolTip1.ReshowDelay = 250;
             this.toolTip1.ShowAlways = true;
+            // 
+            // button7
+            // 
+            this.button7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button7.Location = new System.Drawing.Point(371, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(42, 24);
+            this.button7.TabIndex = 101;
+            this.button7.Text = "Sort";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button8.Location = new System.Drawing.Point(413, 0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 24);
+            this.button8.TabIndex = 102;
+            this.button8.Text = "Unique";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
@@ -984,6 +1012,34 @@ namespace RegExHelper
         {
             if (!match_mode.Checked)
                 match_mode.Checked = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string[] lines = input_textbox.Text.Split(new string[] {  Environment.NewLine }, StringSplitOptions.None);
+            Array.Sort(lines);
+            output_textbox.Text = String.Join(Environment.NewLine, lines);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string[] lines = input_textbox.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            string templine = null;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] == templine)
+                {
+                    lines[i] = null;
+                }
+                else
+                {
+                    templine = lines[i];
+                }
+            }
+            System.Collections.Generic.List<string> sc = new System.Collections.Generic.List<string>(lines);
+            sc.RemoveAll( x => x == null );
+
+            output_textbox.Text = String.Join(Environment.NewLine, sc);
         }
     }
 
